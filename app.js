39827877614d53747ae59f45d6cb714b71b62233
Eamon <eamon0989@gmail.com>
2021-07-31 19:30:29 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+let average = {
+  JS101: 173,
+  JS120: 105,
+  JS130: 70,
+  JS170171: 28,
+  JS175: 55,
+  JS180181: 49,
+  JS185: 12,
+  Backend: 492,
+};
+
+let hours = document.getElementById("hoursperweek");
+
+function myFunction() {
+  document.getElementById("hoursperweek");
+}
+console.log(hours);
